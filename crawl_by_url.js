8ed9df9e09fca1652ce7a8101b609b5dfaa5ed3e
Tsung <d5269357812@gmail.com>
2020-01-11 17:48:13 +0800

    bug:null point

--- crawl_by_url.js
+++ crawl_by_url.js
@@ -19,11 +19,12 @@ let Interval = setInterval(() => {
     }
     //當頁面在店家頁面內
     else {
-        //若此業商家名稱 和 列表最後一行 名稱一樣 就不加入
-        if (rows[rows.length-1][0] == getElementByXpath('//*[@id="pane"]/div/div[1]/div/div/div[2]/div[1]/div[1]/h1').innerHTML){
-            return
+        if(rows!=0){
+            //若此業商家名稱 和 列表最後一行 名稱一樣 就不加入
+            if (rows[rows.length-1][0] == getElementByXpath('//*[@id="pane"]/div/div[1]/div/div/div[2]/div[1]/div[1]/h1').innerHTML){
+                return
+            }
         }
-
         rows.push(get_store_info_row())
         store_num++
     }
